27df3b48dc3f8263f2ff7589acc1256273ec3a1d
denisatux <denise.gonzalez@finagil.com.mx>
2021-02-05 18:00:01 -0600

    Cambio visor pdf

--- PLD/Confidencialidad2.aspx.vb
+++ PLD/Confidencialidad2.aspx.vb
@@ -17,15 +17,39 @@ Public Class Confidencialidad2
         rptCARTA.SetParameterValue("nombre", nombre)
         rptCARTA.SetParameterValue("empresa", empresa)
         rptCARTA.SetParameterValue("equipo", equipo)
-        Dim rutaPDF As String = "\\Server-nas\tmpfinagil\PLD\" & nombre & ".pdf"
+        'Dim NOMBREX As String = FormatDateTime(Of Date,1)()
+
+
+        Dim rutaPDF As String = FormatDateTime(Now)
+        rutaPDF = rutaPDF.Replace("/", "")
+        rutaPDF = rutaPDF.Replace(".", "")
+        rutaPDF = rutaPDF.Replace(" ", "")
+        rutaPDF = rutaPDF.Replace(":", "")
+        Dim nombrepdf As String = rutaPDF & ".pdf"
+        rutaPDF = "~/tmp/" & rutaPDF & ".pdf"
+
+
+
+        'Dim rutaPDF As String = "\\Server-nas\tmpfinagil\PLD\" & nombre & ".pdf"
         Dim rutaPDF2 As String = Server.MapPath(rutaPDF)
-        rptCARTA.ExportToDisk(ExportFormatType.PortableDocFormat, "\\Server-nas\tmpfinagil\PLD\" & nombre & ".pdf")
-        Response.Redirect("~/PLD/verPdf.aspx", True)
-        'Response.Write("<script>")
-        'Response.Write("window.open('verPdf.aspx','popup','_blank','width=200,height=200')")
-        'Response.Write("</script>")
+        rptCARTA.ExportToDisk(ExportFormatType.PortableDocFormat, rutaPDF2)
+        'Dim RUTA2 As String = "~/tmp/" & Date.Now & Hour(Now) & " : " & Minute(Now) & " : " & Second(Now) & ".pdf"
+
+
         rptCARTA.Dispose()
         'Response.Redirect("close.html")
+        My.Computer.FileSystem.CopyFile(Server.MapPath(rutaPDF), "\\Server-nas\tmpfinagil\PLD\" & nombre & ".pdf", True)
+
+
+
+        If Not Me.IsPostBack Then
+            Dim filePath As String = nombrepdf '"~/Procesados/" & Request.QueryString("fileName") & ".pdf"
+            Response.ContentType = "Application/pdf"
+            Response.AddHeader("Content-disposition", "attachment; filename=" & filePath)
+            Response.TransmitFile(Server.MapPath(rutaPDF))
+            Response.[End]()
+        End If
+
 
     End Sub
 
